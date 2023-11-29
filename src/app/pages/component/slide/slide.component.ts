35d42dce1586788f112fb34171c63414555c5fdc
Tufavo1 <jo.romeroh@duocuc.cl>
2023-11-29 15:28:10 -0300

    2da actualizacion

---
+++ src/app/pages/component/slide/slide.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { IonicSlides } from '@ionic/core';
+import { DataService } from 'src/app/services/shared-data/data.service';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Component({
+  selector: 'app-slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SlideComponent implements OnInit {
+  @Input() slides: any[] = [];
+  swiperModules = [IonicSlides];
+  @ViewChild('Swiper')
+  swiperRef: ElementRef | undefined;
+  location: string = 'Ubicación no disponible';
+  constructor(
+    private data: DataService
+  ) { }
+
+  ngOnInit() {
+    this.getCurrentLocation();
+  }
+
+  async getCurrentLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    const location = `Lat: ${coordinates.coords.latitude}, Lng: ${coordinates.coords.longitude}`;
+    this.data.setLocation(location);
+  }
+
+  onSlideChange(event: any) {
+    console.log(this.swiperRef?.nativeElement.swiper.activeIndex);
+    console.log('event', event);
+  }
+
+}
